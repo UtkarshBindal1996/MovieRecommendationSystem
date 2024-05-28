8a1f06cdfd67366cdc05c417f84308d6634ff1f8
utkarsh <utkarshbindal1996@gmail.com>
2024-05-28 19:02:55 +0530

    fixing bugs in new streamlit app

--- new_streamlitapp.py
+++ new_streamlitapp.py
@@ -4,31 +4,32 @@ import pickle
 import numpy as np
 import datetime
 
-# Fetching the stored data.
-rating_data_path = "./data/rating.csv"
-rating_df = pd.read_csv(rating_data_path)
+@st.cache_data
+def load_data():
+    # Data fetch
+    rating_df = pd.read_csv("./data/rating.csv")
+    movie_df = pd.read_csv("./data/movie.csv")
+    with open('./data/similarity_matrix.pkl', 'rb') as f:
+        similarity_matrix = pickle.load(f)
+    with open('./data/clustered_data.pkl', 'rb') as f:
+        clustered_df = pickle.load(f)
 
-movie_data_path = "./data/movie.csv"
-movie_df = pd.read_csv(movie_data_path)
+    # Data preprocess
+    rating_df['timestamp'] = pd.to_datetime(rating_df['timestamp'])
+    rating_df = rating_df[rating_df['timestamp'].dt.year >= 2014]
 
-# Filtering out the data.
-rating_df['timestamp'] = pd.to_datetime(rating_df['timestamp'])
-rating_df = rating_df[rating_df['timestamp'].dt.year >= 2010]
+    # Filtering the relevant movies
+    unique_movies_id = rating_df['movieId'].unique().tolist()
+    unique_movies_names = []
+    movie_df = movie_df.set_index('movieId')
+    for movie in unique_movies_id:
+        if movie < len(movie_df):
+            unique_movies_names.append(movie_df.iloc[movie]["title"])
+    unique_movies_names.sort()
 
-
-# Filtering the relevant movies
-unique_movies_id = rating_df['movieId'].unique().tolist()
-unique_movies_names = []
-movie_df = movie_df.set_index('movieId')
-for movie in unique_movies_id:
-    unique_movies_names.append(movie_df.iloc[movie]["title"])
-
-# Fetching the simiilarity matrix
-with open('./data/similarity_matrix.pkl', 'rb') as f:
-    similarity_matrix = pickle.load(f)
-
-with open('./data/clustered_data.pkl', 'rb') as f:
-    clustered_df = pickle.load(f)
+    return rating_df, movie_df, similarity_matrix, clustered_df, unique_movies_names
+# Load data using the cached function
+rating_df, movie_df, similarity_matrix, clustered_df, unique_movies_names = load_data()
 
 # Adjusted cosine similarity
 # function to compute similarity between 2 users
@@ -62,10 +63,6 @@ def adjusted_cosine_similarity(user1_ratings, user2_ratings, min_ratings=7):
         time_difference_days = time_difference.days
         penalty = 1 - decay_rate * time_difference_days
         return max(penalty, 0)  # Ensure penalty is between 0 and 1
-    
-    # numerator = sum((user1_ratings[movie] - user1_avg) * (user2_ratings[movie] - user2_avg) for movie in intersection)
-    # denominator = np.sqrt(sum((user1_ratings[movie] - user1_avg)**2 for movie in intersection) * 
-    #                      sum((user2_ratings[movie] - user2_avg)**2 for movie in intersection))
 
     numerator = sum([(user1_ratings[movie][0] - user1_avg) * (user2_ratings[movie][0] - user2_avg) *
                    freshness_penalty(user1_ratings[movie][1] - user2_ratings[movie][1]) for movie in intersection])
@@ -81,10 +78,20 @@ def adjusted_cosine_similarity(user1_ratings, user2_ratings, min_ratings=7):
 user_ratings = {}
 
 def recommend_movies(user_ratings):
-    # Find most similar user in training data (assuming similarity scores are stored somewhere)
-    most_similar_user_index = np.argmax(similarity_matrix, axis=0)  # Find index of max similarity
+
+    if len(user_ratings) == 0:
+        return
+    
+    # Calculate similarity with all users in training data
+    similarities = []
+    for user_index in range(len(similarity_matrix)):
+        similarity = adjusted_cosine_similarity(user_ratings, clustered_df.loc[user_index, 'movie_rating_dict'])
+        similarities.append(similarity)
+    # Find user with maximum similarity
+    most_similar_user_index = np.argmax(similarities)
+
     # Predict cluster label based on most similar user's cluster
-    predicted_cluster = clustered_df.loc[most_similar_user_index, 'cluster_id'].iloc[0]
+    predicted_cluster = clustered_df.loc[most_similar_user_index, 'cluster_id']
     recommendations = {}
 
     # Need to recommend high rated movies from the same cluster.
@@ -96,13 +103,13 @@ def recommend_movies(user_ratings):
     result_dict = {}
     for _, row in single_cluster_df.iterrows():
         similarity_score = adjusted_cosine_similarity(row['movie_rating_dict'], user_ratings, min_ratings=1)
-        for movieId, rating in row['movie_rating_dict'].items():
+        for movieId, value in row['movie_rating_dict'].items():
             if movieId not in result_dict:
                 result_dict[movieId] = {
                     "sum_similarity_rating": 0,
                     "sum_similarity": 0
                 }
-            result_dict[movieId]["sum_similarity_rating"] += similarity_score*rating
+            result_dict[movieId]["sum_similarity_rating"] += (similarity_score*value[0])
             result_dict[movieId]["sum_similarity"] += similarity_score
 
             if result_dict[movieId]["sum_similarity"] == 0:
@@ -110,6 +117,8 @@ def recommend_movies(user_ratings):
             else:
                 net_score = result_dict[movieId]["sum_similarity_rating"]/result_dict[movieId]["sum_similarity"]
             recommendations[movieId] = net_score
+    
+    return recommendations
 
 st.title("Movie Mate: Find your next cinematic match")
 st.write("Simply rate a couple of movies from the list below and hit get recomendation!")
@@ -118,32 +127,35 @@ st.write("Simply rate a couple of movies from the list below and hit get recomen
 all_ratings = []  # List to store all movie ratings
 
 movies_to_rate = st.multiselect("Select movies you want to rate:", unique_movies_names)
-submit_button_disabled = not movies_to_rate  # Initially disabled if no movies selected
+# submit_button_disabled = not movies_to_rate  # Initially disabled if no movies selected
 
 for movie in movies_to_rate:
-  # Check if user selected a movie
-  if movie:
-    movie_column, rating_column = st.columns(2)
-    with movie_column:
-      st.write(f"Movie: {movie}")  # Display selected movie for clarity
-    with rating_column:
-      rating_select = rating_column.slider(f"Rate {movie}:", 1, 5, 1)
-    all_ratings.append((movie, rating_select))  # Add rating to list
-    submit_button_disabled = False
+    # Check if user selected a movie
+    if movie:
+        movie_column, rating_column = st.columns(2)
+        with movie_column:
+            st.write(f"Movie: {movie}")  # Display selected movie for clarity
+        with rating_column:
+            rating_select = rating_column.slider(f"Rate {movie}:", 1, 5, 1)
+        all_ratings.append((movie, rating_select))  # Add rating to list
+        submit_button_disabled = False
 
-if st.button("Submit Ratings", disabled=submit_button_disabled):
+if st.button("Submit Ratings"):
+
     # Update user_ratings dictionary from all_ratings list
     for movie, rating in all_ratings:
         movie_id = movie_df[movie_df['title'] == movie].index[0]
         user_ratings[movie_id] = [rating, datetime.datetime.now()]
+    
+    # Recommendation section
+    recommendations = recommend_movies(user_ratings)
 
-# Recommendation section
-recommendations = recommend_movies(user_ratings)
-
-# Sorting the results
-sorted_movies = dict(sorted(recommendations.items(), key=lambda item: item[1], reverse=True))
-# Fetching top 5 movies
-top_5_movies = list(sorted_movies.keys())[:5]
-st.subheader("Recommended Movies for You:")
-for movie in top_5_movies:
-    st.write(movie_df.iloc[movie]["title"])
+    # Sorting the results
+    sorted_movies = {}
+    if recommendations and len(recommendations) != 0:
+        sorted_movies = dict(sorted(recommendations.items(), key=lambda item: item[1], reverse=True))
+        # Fetching top 5 movies
+        top_5_movies = list(sorted_movies.keys())[:5]
+        st.subheader("Recommended Movies for You:")
+        for movie in top_5_movies:
+            st.write(movie_df.iloc[movie]["title"])
